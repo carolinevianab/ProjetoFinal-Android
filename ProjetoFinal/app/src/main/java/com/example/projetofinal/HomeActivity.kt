a7175ab5f9611e8f63fc858877508b5655741896
carolinevianab <carolinevb123@gmail.com>
2021-06-03 11:41:29 -0300

    Home created

---
+++ ProjetoFinal/app/src/main/java/com/example/projetofinal/HomeActivity.kt
@@ -0,0 +1,16 @@
+package com.example.projetofinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.projetofinal.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+    lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+    }
+}
