c99dbc7c7e96060cb0e613fc1fe9195d3e9cdc83
carolinevianab <carolinevb123@gmail.com>
2021-06-04 21:23:09 -0300

    Authentication I guess

--- ProjetoFinal/app/src/main/java/com/example/projetofinal/MainActivity.kt
+++ ProjetoFinal/app/src/main/java/com/example/projetofinal/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.projetofinal
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.projetofinal.databinding.ActivityMainBinding
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -12,14 +17,41 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnCreateAccount.setOnClickListener {
-            var createAccount = Intent(this, CreateAccountActivity::class.java)
-            startActivity(createAccount)
+        if (getUser() == null) {
+            binding.btnLogin.setOnClickListener {
+                val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
+                val intent = AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build()
+
+                // Apparently, startActivityForResult is deprecated
+                // Now, this is the new way
+                startForResult.launch(intent)
+            }
+        }
+        else {
+            goToHome()
+        }
+    }
+
+    val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result ->
+        if (result.resultCode == Activity.RESULT_OK){
+            val data: Intent? = result.data
+            // Faz algo
+            goToHome()
         }
 
-        binding.btnLogin.setOnClickListener {
-            var login = Intent(this, HomeActivity::class.java)
-            startActivity(login)
-        }
+    }
+
+    fun goToHome(){
+        val login = Intent(this, HomeActivity::class.java)
+        login.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(login)
+        finish()
+    }
+
+    fun getUser(): FirebaseUser? {
+        return FirebaseAuth.getInstance().currentUser
     }
 }
